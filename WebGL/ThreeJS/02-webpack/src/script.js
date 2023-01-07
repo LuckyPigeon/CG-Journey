89af7dfcfa2067592a22d9ba3f2625a1b86ce912
LuckyPigeon <lucky90322@gmail.com>
2023-01-08 01:38:45 +0800

    add three webpack

---
+++ WebGL/ThreeJS/02-webpack/src/script.js
@@ -0,0 +1,14 @@
+/**
+ * 02 Webpack
+ * This example shows how to use Webpack compile whole ThreeJS project,
+ * and use npm run to deploy the project. This is an optional lecture,
+ * you may look into "webpack.common.js" and "webpack.dev.js" file,
+ * and google the options if you ecounter some confuse part.
+ * You may escape it and see the next lecture.
+ */
+
+import * as THREE from 'THREE';
+
+import './style.css'
+
+console.log(THREE)
